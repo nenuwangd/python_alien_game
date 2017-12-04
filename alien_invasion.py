ff2f3d59a8830059c9f48e9e17f5ae30a475d4e8
nenuwangd <nenuwangd@hotmail.com>
2017-12-04 16:59:26 +0800

    打飞机游戏0.2。 添加了外星人，能够消灭外星人；添加了游戏状态，增加了一些游戏设置

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,9 +2,12 @@ import  sys
 import  pygame
 
 from settings import  Settings
+from game_stats import  GameStats
 from ship import  Ship
+from alien import Alien
 import  game_functions as gf
 from pygame.sprite import  Group
+
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -17,14 +20,22 @@ def run_game():
     ship  = Ship(ai_settings,screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
-
+    #创建一个外星人
+    alien = Alien(ai_settings,screen)
+    #创建外星人群
+    aliens = Group()
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
 #    开始游戏的主循环
     while True:
         #监控鼠标\键盘事件
         gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
         #print(len(bullets))
         #重绘屏幕
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 run_game()
--- settings.py
+++ settings.py
@@ -6,11 +6,17 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-        #飞机移动速度
+        #飞船设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
         # 子弹设置
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 30
+        # fleet_direction为1表示向右移，为-1表示向左移
+        self.fleet_direction = 1
